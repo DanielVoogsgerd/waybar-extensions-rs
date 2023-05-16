b6b88a6b35a93fcaa19ace71493c659f964eb7dc
Daniel Voogsgerd <daniel@voogsgerd.nl>
2023-05-16 17:33:59 +0200

    feat(github): Add github notifications

---
+++ src/bin/github-notifications.rs
@@ -0,0 +1,85 @@
+use std::time::Duration;
+
+use itertools::Itertools;
+use serde_json::json;
+use tokio::process::Command;
+use waybar_extensions::waybar::WaybarResponse;
+
+#[tokio::main]
+async fn main() {
+    use octocrab::Octocrab;
+
+    // let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+    let token = String::from_utf8(
+        Command::new("pass")
+            .arg("Background/GitHub")
+            .output()
+            .await
+            .expect("Could not get Github token")
+            .stdout,
+    )
+    .expect("Github token is not valid UTF-8")
+    .trim_end()
+    .to_owned();
+
+    let octocrab = Octocrab::builder()
+        .personal_token(token)
+        .build()
+        .expect("Could not create octocrab instance");
+
+    loop {
+        if let Ok(notifications) = octocrab.activity().notifications().list().send().await {
+            let text = format!("{}", notifications.items.len());
+
+            let tooltip = notifications
+                .items
+                .iter()
+                .map(|notification| {
+                    format!(
+                        "{}  {}",
+                        get_icon(&notification.subject.r#type),
+                        notification.subject.title
+                    )
+                })
+                .join("\n");
+
+            let mut class = vec![];
+
+            if !notifications.items.is_empty() {
+                class.push(String::from("active"));
+            } else {
+                class.push(String::from("passive"));
+            }
+
+            println!(
+                "{}",
+                json!(WaybarResponse {
+                    text,
+                    tooltip,
+                    class,
+                })
+            );
+        } else {
+            println!(
+                "{}",
+                json!(WaybarResponse {
+                    text: String::from(""),
+                    tooltip: String::from("Could not request notifications"),
+                    class: vec![String::from("warning")],
+                })
+            );
+        }
+
+        tokio::time::sleep(Duration::from_secs(300)).await;
+    }
+}
+
+fn get_icon(x: &str) -> &str {
+    match x {
+        "PullRequest" => "",
+        "Issue" => "",
+        "Discussion" => "",
+        "Release" => "",
+        y => y,
+    }
+}
